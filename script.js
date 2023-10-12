89bc8fd1112d67f082bf62d3f526186af76dedb2
AdityaGhamat <127591208+AdityaGhamat@users.noreply.github.com>
2023-10-12 10:27:31 -0700

    updated project 3rd time

--- script.js
+++ script.js
@@ -1,17 +1,34 @@
+//API-KEY=Asws2gmY-yRt-ySjgOmqWlyA4YJPJ5aOriqlrftYJ41dT5Pm5jnntksr1qZz-pCb    
+//this is map key.
 var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    grabCursor:true,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+//hamburger
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector("#nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
